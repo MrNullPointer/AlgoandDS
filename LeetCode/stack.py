de899701629ed8577ae0eff10d25d4ce4a4fbaed
iparikshitdubey <peestuck@gmail.com>
2020-02-17 16:00:44 -0500

    Basic stack functioning

---
+++ LeetCode/stack.py
@@ -0,0 +1,11 @@
+stack = []
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print("The initial stack is: ", stack)
+
+print("Popping the stack one by one")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
