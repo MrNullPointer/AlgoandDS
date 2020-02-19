dd4895529df5f21d70642b467e0d5142813a45a3
iparikshitdubey <peestuck@gmail.com>
2020-02-18 19:33:21 -0500

    A wrapper stack class

--- LeetCode/stack.py
+++ LeetCode/stack.py
@@ -1,11 +1,25 @@
-stack = []
-stack.append('a')
-stack.append('b')
-stack.append('c')
+class stack(object):
+    def __init__(self):
+        self.stack = []
+    def push(self,x):
+        self.stack.append(x)
+    def pop(self):
+        if len(self.stack) > 0:
+            self.stack.pop()
+        else:
+            return None
+    def peek(self):
+        if len(self.stack) > 0:
+            return self.stack[len(self.stack) - 1]
+        else:
+            return None
+    def __str__(self):
+        return str(self.stack)
 
-print("The initial stack is: ", stack)
 
-print("Popping the stack one by one")
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
+my_stack = stack()
+my_stack.pop()
+my_stack.push(2)
+my_stack.push(4)
+print(my_stack.peek())
+print(my_stack)
