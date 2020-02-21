cb9b4f217660d64ac2b6b40bf5674b5e35c10958
iparikshitdubey <peestuck@gmail.com>
2020-02-20 19:53:13 -0500

    Runtime: 32 ms, faster than 67.63% ||| Memory Usage: 12.9 MB, less than 100.00%

---
+++ LeetCode/Problem202.py
@@ -0,0 +1,41 @@
+'''HAPPY NUMBER
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example:
+
+Input: 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1'''
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        num = n
+        count = 0
+
+        if num is 0:
+            return False
+        if num is 1:
+            return True
+
+        while count < 50:
+            newList = []
+            num1 = 0
+            while True:
+                newList.append(num % 10)
+                num = num // 10
+                if num == 0:
+                    break
+            for elem in newList:
+                num1 = num1 + (elem * elem)
+            num = num1
+            count += 1
+            if num == 1:
+                return True
+
